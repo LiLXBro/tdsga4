785209bad11dbcefc5e94a18ac58f2ede5cb53f7
shashi kumar singh <shashikumarsingh794@gmail.com>
2026-07-21 11:00:14 +0530

    first2

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # The only two things you must fill in:
-EMAIL = "your_email@ds.study.iitm.ac.in"
+EMAIL = "23f2003619@ds.study.iitm.ac.in"
 AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjIwMDM2MTlAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDYwNzQxMSwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTIxMjIxMX0.9Tgz8oblKie2ZS-_D50vif0bFTzbtD-mk0UECHXCQk0"
 
 # Fixed — do not change
